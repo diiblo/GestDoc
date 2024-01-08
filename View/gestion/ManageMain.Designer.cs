e89d55f8fc26563363307fb87197443224929314
diiblo <basirukingrahman98@gmail.com>
2024-01-08 01:24:58 +0100

    ManageMain || Begin

--- View/gestion/ManageMain.Designer.cs
+++ View/gestion/ManageMain.Designer.cs
@@ -29,24 +29,154 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.kryptonPaletteMain = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.uiFlowLayoutPanel1 = new Sunny.UI.UIFlowLayoutPanel();
+            this.kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.codeeloPictureBox1 = new CodeeloUI.Controls.CodeeloPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.codeeloPictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // kryptonPaletteMain
+            // 
+            this.kryptonPaletteMain.ButtonSpecs.FormClose.Image = global::DelotransApp.Properties.Resources.icons8_cancel_red_16;
+            this.kryptonPaletteMain.ButtonSpecs.FormClose.ImageStates.ImagePressed = global::DelotransApp.Properties.Resources.icons8_cancel_redClair_16;
+            this.kryptonPaletteMain.ButtonSpecs.FormClose.ImageStates.ImageTracking = global::DelotransApp.Properties.Resources.icons8_cancel_redClair_16;
+            this.kryptonPaletteMain.ButtonSpecs.FormMax.Image = global::DelotransApp.Properties.Resources.icons8_final_state_bleu_16;
+            this.kryptonPaletteMain.ButtonSpecs.FormMax.ImageStates.ImagePressed = global::DelotransApp.Properties.Resources.icons8_final_state_bleuClaire_16;
+            this.kryptonPaletteMain.ButtonSpecs.FormMax.ImageStates.ImageTracking = global::DelotransApp.Properties.Resources.icons8_final_state_bleuClaire_16;
+            this.kryptonPaletteMain.ButtonSpecs.FormMin.Image = global::DelotransApp.Properties.Resources.icons8_minus_orange_16;
+            this.kryptonPaletteMain.ButtonSpecs.FormMin.ImageStates.ImagePressed = global::DelotransApp.Properties.Resources.icons8_minus_orangeClaire_16;
+            this.kryptonPaletteMain.ButtonSpecs.FormMin.ImageStates.ImageTracking = global::DelotransApp.Properties.Resources.icons8_minus_orangeClaire_16;
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StateNormal.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StateNormal.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StateNormal.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StateNormal.Border.Width = 0;
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StatePressed.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StatePressed.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StatePressed.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StatePressed.Border.Width = 0;
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPaletteMain.ButtonStyles.ButtonForm.StateTracking.Border.Width = 0;
+            this.kryptonPaletteMain.FormStyles.FormMain.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
+            this.kryptonPaletteMain.FormStyles.FormMain.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
+            this.kryptonPaletteMain.FormStyles.FormMain.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPaletteMain.FormStyles.FormMain.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            this.kryptonPaletteMain.FormStyles.FormMain.StateCommon.Border.Rounding = 24;
+            this.kryptonPaletteMain.HeaderStyles.HeaderForm.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
+            this.kryptonPaletteMain.HeaderStyles.HeaderForm.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
+            this.kryptonPaletteMain.HeaderStyles.HeaderForm.StateCommon.ButtonEdgeInset = 10;
+            this.kryptonPaletteMain.HeaderStyles.HeaderForm.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, -1, -1, -1);
+            this.kryptonPaletteMain.HeaderStyles.HeaderForm.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(89)))), ((int)(((byte)(178)))));
+            this.kryptonPaletteMain.HeaderStyles.HeaderForm.StateCommon.Content.ShortText.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(89)))), ((int)(((byte)(178)))));
+            this.kryptonPaletteMain.HeaderStyles.HeaderForm.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // uiFlowLayoutPanel1
+            // 
+            this.uiFlowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
+            this.uiFlowLayoutPanel1.FillColor = System.Drawing.Color.White;
+            this.uiFlowLayoutPanel1.FillColor2 = System.Drawing.Color.White;
+            this.uiFlowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uiFlowLayoutPanel1.Location = new System.Drawing.Point(240, 4);
+            this.uiFlowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiFlowLayoutPanel1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiFlowLayoutPanel1.Name = "uiFlowLayoutPanel1";
+            this.uiFlowLayoutPanel1.Padding = new System.Windows.Forms.Padding(2);
+            this.uiFlowLayoutPanel1.Radius = 24;
+            this.uiFlowLayoutPanel1.RectColor = System.Drawing.Color.White;
+            this.uiFlowLayoutPanel1.RectDisableColor = System.Drawing.Color.White;
+            this.uiFlowLayoutPanel1.ScrollBarBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
+            this.uiFlowLayoutPanel1.ScrollBarStyleInherited = false;
+            this.uiFlowLayoutPanel1.ShowText = false;
+            this.uiFlowLayoutPanel1.Size = new System.Drawing.Size(873, 648);
+            this.uiFlowLayoutPanel1.TabIndex = 0;
+            this.uiFlowLayoutPanel1.Text = "uiFlowLayoutPanel1";
+            this.uiFlowLayoutPanel1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Location = new System.Drawing.Point(12, 125);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(188, 57);
+            this.kryptonButton1.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonButton1.StateCommon.Border.Rounding = 12;
+            this.kryptonButton1.StateCommon.Content.Image.ImageH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Near;
+            this.kryptonButton1.StateCommon.Content.Padding = new System.Windows.Forms.Padding(15, -1, -1, -1);
+            this.kryptonButton1.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonButton1.StateCommon.Content.ShortText.TextH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.kryptonButton1.TabIndex = 3;
+            this.kryptonButton1.Values.Image = global::DelotransApp.Properties.Resources.icons8_customer_grey_16;
+            this.kryptonButton1.Values.Text = "Dashboard";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "DELOTRANS";
+            // 
+            // codeeloPictureBox1
+            // 
+            this.codeeloPictureBox1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.codeeloPictureBox1.BorderColorFirst = System.Drawing.Color.RoyalBlue;
+            this.codeeloPictureBox1.BorderColorSecond = System.Drawing.Color.HotPink;
+            this.codeeloPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.codeeloPictureBox1.BorderSize = 2;
+            this.codeeloPictureBox1.DrawBorder = false;
+            this.codeeloPictureBox1.DrawBorderInside = false;
+            this.codeeloPictureBox1.DrawCircle = false;
+            this.codeeloPictureBox1.GradientDirection = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
+            this.codeeloPictureBox1.Image = global::DelotransApp.Properties.Resources.LOGO_removebg_preview;
+            this.codeeloPictureBox1.Location = new System.Drawing.Point(25, 18);
+            this.codeeloPictureBox1.Name = "codeeloPictureBox1";
+            this.codeeloPictureBox1.Size = new System.Drawing.Size(85, 51);
+            this.codeeloPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.codeeloPictureBox1.TabIndex = 5;
+            this.codeeloPictureBox1.TabStop = false;
+            this.codeeloPictureBox1.UseGradient = false;
             // 
             // ManageMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
-            this.ClientSize = new System.Drawing.Size(1193, 666);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ClientSize = new System.Drawing.Size(1126, 666);
+            this.Controls.Add(this.codeeloPictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kryptonButton1);
+            this.Controls.Add(this.uiFlowLayoutPanel1);
             this.Name = "ManageMain";
+            this.Palette = this.kryptonPaletteMain;
             this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "DelotransApp || Manage Document";
+            ((System.ComponentModel.ISupportInitialize)(this.codeeloPictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPaletteMain;
+        private Sunny.UI.UIFlowLayoutPanel uiFlowLayoutPanel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton1;
+        private System.Windows.Forms.Label label1;
+        private CodeeloUI.Controls.CodeeloPictureBox codeeloPictureBox1;
     }
 }
